c9696032e293b95cf2279d2e4fcb2c02ff8c5c6b
Smrutikanta18 <psmrutikanta@gmail.com>
2024-03-24 10:53:19 +0530

    updated

--- app/src/main/java/com/example/contactsmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsmanagerapp/MainActivity.kt
@@ -6,8 +6,6 @@ import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.contactsmanagerapp.databinding.ActivityMainBinding
-import com.example.contactsmanagerapp.databinding.CardItemBinding
-import com.example.contactsmanagerapp.room.UserDAO
 import com.example.contactsmanagerapp.room.UserDatabase
 import com.example.contactsmanagerapp.room.UserRepository
 import com.example.contactsmanagerapp.viewmodel.UserViewModel
@@ -16,13 +14,12 @@ import com.example.contactsmanagerapp.viewmodel.UserViewModelFactory
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var userViewModel: UserViewModel
-    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        button =findViewById(R.id.button)
+        var button:Button =findViewById(R.id.button)
         val dao=UserDatabase.getInstance(application).userDAO
         val repository=UserRepository(dao)
         val factory=UserViewModelFactory(repository)
